6b053661d5c019b6292cef998fc5cf51cea515f2
nimisha-maria <nimishamaria3@gmail.com>
2026-05-29 18:56:02 +0530

    JWT header extraction working

--- src/main/java/resumeanalyzer/security/SecurityConfig.java
+++ src/main/java/resumeanalyzer/security/SecurityConfig.java
@@ -8,9 +8,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -31,6 +35,10 @@ public class SecurityConfig {
                 )
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .formLogin(form -> form.disable());
-        return http.build();
-    }
+        http.addFilterBefore(
+                jwtAuthFilter,
+                UsernamePasswordAuthenticationFilter.class
+        );
+
+        return http.build();    }
 }
